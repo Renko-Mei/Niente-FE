5068ff0d228f3af62a120a37afd204a9a2f5ab77
xwa132 <xwa132@sfu.ca>
2018-06-09 23:46:17 -0700

    Simplist landing page (for now)

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <section class="container">
-    <div>
-      <h1>Niente</h1>
-    </div>
-  </section>
+  <div class="container">
+    <h1>Niente</h1>
+    <h3>
+      布鲁斯·古恩·复仇
+    </h3>
+  </div>
 </template>
 
 <script>
@@ -12,32 +13,12 @@
 
 <style scoped>
 .container {
-  min-height: 100vh;
+  height: 100vh;
   display: flex;
+  flex-flow: column wrap;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  font-size: 30px;
 }
 </style>
