7af5a435c6847379841d80934e07c09d91288b27
xwa132 <xwa132@sfu.ca>
2019-03-06 02:11:30 -0800

    make article edit page work

--- pages/admin/articles/_id.vue
+++ pages/admin/articles/_id.vue
@@ -1,27 +1,33 @@
 <template>
   <div class="admin-post-page">
     <section class="update-form">
-      <newArticleForm :post="loadedPost"/>
+      <newArticleForm :oldArticle="article"/>
     </section>
   </div>
 </template>
 
 <script>
 import newArticleForm from "@/components/articles/newArticleForm.vue";
+import axios from 'axios';
 
 export default {
-  //layout: 'admin',
   components: {
     newArticleForm
   },
+  asyncData({ params, error }) {
+    return axios
+      .get(process.env.baseUrl + '/articles/' + params.id)
+      .then(res => {
+        let article = res.data;
+        return { article };
+			})
+			.catch(e => {
+				error({ statusCode: 404, message: 'Article not found' });
+			});
+  },
   data() {
     return {
-      loadedPost: {
-        author: 'aaa',
-        title: 'My post',
-        content: 'Amazing',
-        thumbnailLink: 'bad link'
-      }
+      article: {}
     }
   }
 };
