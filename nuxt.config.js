1f260e1323e6e305cf87dfaf20118366fd0a370a
xwa132 <xwa132@sfu.ca>
2019-03-09 05:20:59 -0800

    settings update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,76 @@
-module.exports = {
+import * as pkg from './package'
+
+export default {
+  mode: 'universal',
+
   /*
   ** Headers of the page
   */
-  mode: "universal",
   head: {
-    title: 'neu',
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Renko\'s blog' }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
+
   /*
-  ** Customize the progress bar color
+  ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend(config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  },
+
   /*
   ** Global CSS
   */
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+  css: ['element-ui/lib/theme-chalk/index.css'],
+
   /*
-  ** Add element-ui in our app, see plugins/element-ui.js file
+  ** Plugins to load before mounting the App
   */
   plugins: [
     '@/plugins/element-ui',
     '@/plugins/date-filter',
     '@/plugins/axios'
   ],
+
+  /*
+  ** Nuxt.js modules
+  */
   modules: [
-    '@nuxtjs/axios',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios'
   ],
+  /*
+  ** Axios module configuration
+  */
   axios: {
-    baseURL: process.env.BASE_URL || "http://ec2-34-222-33-125.us-west-2.compute.amazonaws.com/api"
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: [/^element-ui/],
+
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
   }
 }
