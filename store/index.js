4d18ee2dba7e8970742509f873e2ae1b392f526e
xwa132 <xwa132@sfu.ca>
2019-03-09 20:48:25 -0800

    Oft when the summer sleeps among the trees, Whisp’ring faint murmurs to the scanty breeze, I walk the village round; if at her side A youth doth walk in stolen joy and pride

--- store/index.js
+++ store/index.js
@@ -1,45 +1,39 @@
-import Vuex from 'vuex';
+export const state = () => ({
+  token: null
+})
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      token: null
-    },
-    mutations: {
-      setJwtToken(state, token) {
-        state.token = token
-      }
-    },
-    actions: {
-      async nuxtServerInit(veuxContext, context) {
-        // try {
-        //   let limit = 50; // auto calculate this value based on screensize?
-        //   let data = await context.app.$axios.$get("/articlepreviews/?limit=" + String(limit));
-        //   veuxContext.commit('setArticlePreviews', data);
-        // }
-        // catch (e) {
-        //   throw e;
-        // }
-      },
-      async login(context, user) {
-        try {
-          let data = await this.$axios.$post('/login/', user);
-          context.commit('setJwtToken', data);
-        }
-        catch (e) {
-          throw e;
-        };
-      }
-    },
-    getters: {
-      isAuthenticated(state) {
-        return state.token != null;
-      },
-      jwtToken(state) {
-        return state.token != null ? state.token : "";
-      }
-    }
-  });
+export const mutations = {
+  setJwtToken(state, token) {
+    state.token = token
+  }
 }
 
-export default createStore;
+export const actions = {
+  // async nuxtServerInit(veuxContext, context) {
+  //   try {
+  //     let limit = 50; // auto calculate this value based on screensize?
+  //     let data = await context.app.$axios.$get("/articlepreviews/?limit=" + String(limit));
+  //     veuxContext.commit('setArticlePreviews', data);
+  //   }
+  //   catch (e) {
+  //     throw e;
+  //   }
+  // },
+  async login(context, user) {
+    try {
+      const data = await this.$axios.$post('/login/', user)
+      context.commit('setJwtToken', data)
+    } catch (e) {
+      throw e
+    };
+  }
+}
+
+export const getters = {
+  isAuthenticated(state) {
+    return state.token != null
+  },
+  jwtToken(state) {
+    return state.token != null ? state.token : ''
+  }
+}
